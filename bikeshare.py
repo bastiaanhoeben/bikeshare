3dcba7546a2071a4976fc158b7683a432fe1976d
Bastiaan Hoeben <bastiaan.hoeben@digital-power.com>
2021-05-24 12:18:21 +0200

    Clean up project repository
    The csv data files are moved to a separate data directory.
    further-learning.txt file is removed as it does not relate to this project.
    A requirements.txt file is added for setting up a venv.

--- bikeshare.py
+++ bikeshare.py
@@ -6,9 +6,9 @@ import pandas as pd
 # prevent collapsing of dataframe columns in output
 pd.set_option('display.max_columns', 200)
 
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
+CITY_DATA = { 'chicago': 'data/chicago.csv',
+              'new york city': 'data/new_york_city.csv',
+              'washington': 'data/washington.csv' }
 
 def get_filters():
     """
